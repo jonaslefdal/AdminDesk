45bca4931c541d36218d19ef6d3791b79fc7b386
jonaslefdal <jonas.lefdal@gmail.com>
2023-11-27 16:52:54 +0100

    Merge issue

--- AdminDesk/Startup.cs
+++ AdminDesk/Startup.cs
@@ -76,6 +76,31 @@ public class Startup
             app.UseHsts();
         }
 
+
+        app.Use(async (context, next) =>
+        {
+
+            context.Response.Headers.Add("X-Xss-Protection", "1");
+            context.Response.Headers.Add("X-Frame-Options", "DENY");
+            context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Add(
+                "Content-Security-Policy",
+                "default-src 'self'; " +
+                "img-src 'self'; " +
+                "font-src 'self'; " +
+                "style-src 'self'; " +
+                "script-src 'self'" +
+                "frame-src 'self';" +
+                "connect-src 'self';");
+            await next();
+        });
+
+        // To enforce HTTPS connection
+        app.UseHsts();
+
+
+
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
