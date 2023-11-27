076873b1356a32fe8425af2c144562be1620a3e5
jonaslefdal <jonas.lefdal@gmail.com>
2023-11-27 17:33:48 +0100

    Css

--- AdminDesk/Startup.cs
+++ AdminDesk/Startup.cs
@@ -76,28 +76,28 @@ public class Startup
             app.UseHsts();
         }
 
+ /*       
+app.Use(async (context, next) =>
+            {
 
-        app.Use(async (context, next) =>
-        {
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                context.Response.Headers.Add("X-Frame-Options", "DENY");
+                context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add(
+                    "Content-Security-Policy",
+                    "default-src 'self'; " +
+                    "img-src 'self'; " +
+                    "font-src 'self'; " +
+                    "style-src 'self'; " +
+                    "script-src 'self'" +
+                    "frame-src 'self';" +
+                    "connect-src 'self';");
+                await next();
+            });*/
 
-            context.Response.Headers.Add("X-Xss-Protection", "1");
-            context.Response.Headers.Add("X-Frame-Options", "DENY");
-            context.Response.Headers.Add("Referrer-Policy", "no-referrer");
-            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-            context.Response.Headers.Add(
-                "Content-Security-Policy",
-                "default-src 'self'; " +
-                "img-src 'self'; " +
-                "font-src 'self'; " +
-                "style-src 'self'; " +
-                "script-src 'self'" +
-                "frame-src 'self';" +
-                "connect-src 'self';");
-            await next();
-        });
-
-        // To enforce HTTPS connection
-        app.UseHsts();
+            // To enforce HTTPS connection
+            app.UseHsts(); 
 
 
 
